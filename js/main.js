8520fa32c087b32b90e9cf6aa596d034a6a550e1
Rasmus Andersson <90144367+rasmus-ason@users.noreply.github.com>
2022-04-13 13:31:36 +0200

    Kod

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function hamburger_menu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//Sticky navbar
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyNavbar()};
+
+// Get the navbar
+var navbar = document.getElementById("stickyheader");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
